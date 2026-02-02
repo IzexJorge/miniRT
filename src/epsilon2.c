4a90980b2adf8c60290507495007f18b94347455
Jorge <Izex_jorge@hotmail.com>
2026-02-02 01:02:09 +0100

    Arreglado problema de ver planos cuando la cámara está justo en su superficie. Falta hacer lo mismo para los demás objetos. Mañana lo hago.

--- src/epsilon2.c
+++ src/epsilon2.c
@@ -6,7 +6,7 @@
 /*   By: jescuder <jescuder@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 20:26:37 by jescuder          #+#    #+#             */
-/*   Updated: 2025/11/07 20:47:58 by jescuder         ###   ########.fr       */
+/*   Updated: 2026/02/02 00:08:49 by jescuder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 int	ft_is_zero(double a)
 {
-	return (fabs(a) < EPSILON);
+	return (fabs(a) <= EPSILON);
 }
--- src/plane.c
+++ src/plane.c
@@ -6,7 +6,7 @@
 /*   By: jescuder <jescuder@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 19:33:25 by jose-jim          #+#    #+#             */
-/*   Updated: 2025/11/24 09:34:12 by jescuder         ###   ########.fr       */
+/*   Updated: 2026/02/02 00:53:08 by jescuder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@ int	intersect_disk(t_ray ray, t_cylinder *cy, t_vec3 center, double *t)
 	t_vec3	cp;
 
 	denom = vec3_dot(ray.dir, cy->axis);
-	if (fabs(denom) < EPSILON)
+	if (ft_is_zero(denom))
 		return (0);
 	tmp = vec3_dot(vec3_sub(center, ray.origin), cy->axis) / denom;
-	if (ft_is_less(tmp, 0.0))
+	if (tmp < 0.0 || ft_is_zero(tmp))
 		return (0);
 	hit = vec3_add(ray.origin, vec3_scale(ray.dir, tmp));
 	cp = vec3_sub(hit, center);
-	if (vec3_length(cp) <= cy->diameter * 0.5)
+	if (ft_is_less_equal(vec3_length(cp), cy->diameter * 0.5))
 	{
 		*t = tmp;
 		return (1);
@@ -41,13 +41,13 @@ static int	ft_intersect_plane(t_ray ray, t_plane *pl, double *t)
 	t_vec3	p0r0;
 
 	denom = vec3_dot(pl->normal, ray.dir);
-	if (fabs(denom) > EPSILON)
-	{
-		p0r0 = vec3_sub(pl->coord, ray.origin);
-		*t = vec3_dot(p0r0, pl->normal) / denom;
-		return (ft_is_greater_equal(*t, 0.0));
-	}
-	return (0);
+	if (ft_is_zero(denom))
+		return (0);
+	p0r0 = vec3_sub(pl->coord, ray.origin);
+	*t = vec3_dot(p0r0, pl->normal) / denom;
+	if (*t < 0.0 || ft_is_zero(*t))
+		return (0);
+	return (1);
 }
 
 void	ft_search_planes(t_ray ray, t_scene *scene, double *min_t, t_hit *hit)
